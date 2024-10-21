5c9879244d4c765e084ffb35dc4ece098934c9b5
Chirayu <chirayusavaria@gmail.com>
2024-10-21 21:42:09 +0530

    New files added

---
+++ datatypes/01_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"// treat all JS code a s newer version(automaticallyused internally by engine)
+
+// alert(3+3); // we are using nodejs, not browser
+
+let fname = "Chirayu";
+let age = 27;
+let isLoggedIn = false;
+let state = null;//empty
+
+//primitive-datatypes: number, String, boolean, bigInt, 
+//null => standalone value
+// undefined => value is not assigned
+//symbol => to find uniqueness in react component
+
+//object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);// object
+
+
